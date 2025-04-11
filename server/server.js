18f2d9ffda9dc544799902e0422ef1288d2ada4e
skyzo-free <ayalatifi6@gmail.com>
2025-04-11 04:16:56 +0100

    create backend

---
+++ server/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";    
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
