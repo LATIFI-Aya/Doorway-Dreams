9689ea14996941100df7fcff74984e831c840dc2
skyzo-free <ayalatifi6@gmail.com>
2025-04-13 04:28:01 +0100

    create Loader

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Loader = () => {
+  return (
+    <div className='flexCenter h-screen w-full'> 
+        <div className='h-24 w-24 border-t-transparent border-blue-500 rounded-full animate-spin '/>
+    </div>
+  )
+}
